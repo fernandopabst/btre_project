3a1f9d0362dccb7d0da5da4e9f4c3f3033e45184
Standalone <Standalone@DL00100.internal.uwic.ac.uk>
2021-04-11 18:42:36 +0100

    added local

--- btreproject/settings.py
+++ btreproject/settings.py
@@ -138,3 +138,8 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .localsettings import *
+except ImportError:
+    pass
